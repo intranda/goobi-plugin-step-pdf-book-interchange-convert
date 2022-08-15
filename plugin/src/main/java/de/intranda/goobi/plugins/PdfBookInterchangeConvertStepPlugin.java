7e5538ec0f106aee615be5a25cd127a842044665
Michael Lippold <michael.lippold@intranda.com>
2022-08-15 11:29:45 +0200

    initial commit after renaming template

--- plugin/src/main/java/de/intranda/goobi/plugins/PdfBookInterchangeConvertStepPlugin.java
+++ plugin/src/main/java/de/intranda/goobi/plugins/PdfBookInterchangeConvertStepPlugin.java
@@ -36,10 +36,10 @@ import net.xeoh.plugins.base.annotations.PluginImplementation;
 
 @PluginImplementation
 @Log4j2
-public class SampleStepPlugin implements IStepPluginVersion2 {
+public class PdfBookInterchangeConvertStepPlugin implements IStepPluginVersion2 {
     
     @Getter
-    private String title = "intranda_step_sample";
+    private String title = "intranda_step_pdf_book_interchange_convert";
     @Getter
     private Step step;
     @Getter
@@ -57,7 +57,7 @@ public class SampleStepPlugin implements IStepPluginVersion2 {
         SubnodeConfiguration myconfig = ConfigPlugins.getProjectAndStepConfig(title, step);
         value = myconfig.getString("value", "default value"); 
         allowTaskFinishButtons = myconfig.getBoolean("allowTaskFinishButtons", false);
-        log.info("Sample step plugin initialized");
+        log.info("PdfBookInterchangeConvert step plugin initialized");
     }
 
     @Override
@@ -70,7 +70,7 @@ public class SampleStepPlugin implements IStepPluginVersion2 {
 
     @Override
     public String getPagePath() {
-        return "/uii/plugin_step_sample.xhtml";
+        return "/uii/plugin_step_pdf_book_interchange_convert.xhtml";
     }
 
     @Override
@@ -109,7 +109,7 @@ public class SampleStepPlugin implements IStepPluginVersion2 {
         boolean successful = true;
         // your logic goes here
         
-        log.info("Sample step plugin executed");
+        log.info("PdfBookInterchangeConvert step plugin executed");
         if (!successful) {
             return PluginReturnValue.ERROR;
         }
